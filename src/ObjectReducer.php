8770ce7c825577bd0c5ab1e94972d5dff2d28b6b
Stephen Hill <stephen@gatekiller.co.uk>
2016-07-26 14:58:18 +0100

    Skeleton ObjectReducer class.

---
+++ src/ObjectReducer.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace StephenHill;
+
+class ObjectReducer
+{
+    private $object;
+    private $maxDepth = 3;
+    private $maxChildren = 128;
+    private $maxStringLength = 512;
+    
+    public function __construct($object)
+    {
+        
+    }
+    
+    public function reduce()
+    {
+        
+    }
+}
